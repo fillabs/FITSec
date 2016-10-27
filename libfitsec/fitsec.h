126f822851d5282acc3a60f21e86c6ca2552598d
Denis Filatov <denis.filatov@fillabs.com>
2016-10-27 16:13:56 +0200

    add some comments to header file

--- libfitsec/fitsec.h
+++ libfitsec/fitsec.h
@@ -1,23 +1,23 @@
 /*********************************************************************
 This file is a part of FItsSec project: Implementation of ETSI TS 103 097
-Copyright (C) 2015  Denis Filatov (danya.filatov()gmail.com)
+Copyright (C) 2015  Denis Filatov (denis.filatov()fillabs.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
 
-This program is distributed under GNU GPLv3 in the hope that it will
+This program is distributed under GNU LGPLv3 in the hope that it will
 be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with Foobar.  If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
-@license GPL-3.0+ <http://www.gnu.org/licenses/gpl-3.0.txt>
+You should have received a copy of the GNU Lesser General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/lgpl-3.0.txt>.
+@license LGPL-3.0+ <http://www.gnu.org/licenses/lgpl-3.0.txt>
 
 In particular cases this program can be distributed under other license
-by simple request to the author.
+by the simple request to the author.
 *********************************************************************/
 
 #ifndef fitsec_h
@@ -93,50 +93,61 @@ extern "C" {
 		FS_USE_CERT_REQUEST = 1,
 		
 		/** send requested AA certificate only if both AA and AT certificate has been requested
-		    from other side and both AA and AT digests are pointed to the currently used certificates */
-		FS_SEND_AA_CERT_IF_BOTH_REQUESTED,
+		    from other side and both AA and AT digests are pointed to the currently used certificates.
+		    The idea is to sign messages by certificate chain only if both AA and AT has been requested.
+		    !This is a non-standard behavior! */
+		FS_SEND_AA_CERT_IF_BOTH_REQUESTED = 1,
 
-		/** request both AT and AA certificates if AA certificate needs to be requested */
-		FS_REQUEST_AT_CERT_WITH_AA,
+		/** request both AT and AA certificates if AA certificate needs to be requested.
+		    !This is a non-standard behavior! */
+		FS_REQUEST_AT_CERT_WITH_AA = 2,
+		
+		/** do not sign next cam with certificate chain if some message signed with chain containing AA certificate was 
+		received after request */
+		FS_CANCEL_AA_REQUEST = 4,
 	};
 
 	typedef struct FitSecConfig
 	{
-		const char                * storage;        // deprecated. path to load and store certificates
+		const char                * storage;        // path to load and store certificates (Deprecated and not used anymore!)
 		int                         hexadecimal;    // certificate data is in hexadecimal form
-		unsigned int                version;        // default protocol version
-		int                         certPeriod;     // set to -1 to do not send certificates automatically,
+		unsigned int                version;        // default protocol version (2)
+		int                         certPeriod;     // set to -1 to do not send certificates in CAM,
 		                                            // set to  0 to send certificates in each CAM
+		                                            // set to 1000 (100 msec) to send each second.
 		unsigned int                flags;          // see FitSecEngineFlags
-		const FSCryptEngineConfig * crypt;          // crypto engine to be used
+		const FSCryptEngineConfig * crypt;          // crypto engine to be used (use openssl by default)
+		FitSec_Event_Fn           * cbOnEvent;      // user callback function to be called when some event has been occured (see FSEventId)
+		void                      * cbUser;         // user pointer to be passed to the callback function
+	} FitSecConfig;
 
-		FitSec_Event_Fn           * cbOnEvent;      // user function to be called when some event has been occured (see FSEventId)
-		void                      * cbUser;         // user pointer to be passed to the function
-	}FitSecConfig;
-
+	/** Initialize the config structure with default values */
 	FITSEC_EXPORT void  FitSecConfig_InitDefault(FitSecConfig * cfg);
 
 	/** Create and initialize engine */
 	FITSEC_EXPORT FitSec *              FitSec_New(const FitSecConfig * config);
 	
-	/** Load certificates from the path cpecified in storage. Deprecated. */
-	/* FITSEC_EXPORT FSBOOL                FitSec_Initialize(FitSec * e); */
 	/** Install certificate (root, AA, local AT pseudonymes) */
 	FITSEC_EXPORT const FSCertificate * FitSec_InstallCertificate(FitSec * e,
 		const char * cert, int cert_length,
 		const char * vkey, int vkey_length,
 		const char * ekey, int ekey_length,
 		int * perror
-		);
+	);
 
 	/** Select certificate to be used as current pseudonym.
 	    This function will trigger the Id changing process */
 	FITSEC_EXPORT FSBOOL                FitSec_Select(FitSec * e, unsigned int cert_id);
+
 	/** Cleanup all data, forget all foreign certificates, 
 	    clean all local certificates if clean_local flag is set */
 	FITSEC_EXPORT void                  FitSec_Clean(FitSec * e, int clean_local);
+	
 	/** Cleanup engine and free all allocated memory */
 	FITSEC_EXPORT void                  FitSec_Free(FitSec * e);
+
+	/** Returns the message corresponding to the error value */
+	FITSEC_EXPORT const char * FitSec_ErrorMessage(int err);
 
 	typedef struct {
 		FSItsAid  aid;
@@ -160,7 +171,7 @@ extern "C" {
 
 	typedef struct FSMessageInfo {
 		int                   status;         // error value. 
-		const char *          errmsg;         // message to describe an error
+		const char *          errmsg;         // error description
 		char *                payload;        
 		int                   payloadSize;
 		FSPayloadType         payloadType;
@@ -173,7 +184,7 @@ extern "C" {
 		
 		// internal data
 		void *_ptrs[8];
-	}FSMessageInfo;
+	} FSMessageInfo;
 
 	/** Sign ITS message.
 	 *  @param e       [In]      The engine
@@ -190,40 +201,40 @@ extern "C" {
 	 *  payload        | out: returns address to copy payload       | in: the same address as returned from FitSec_PrepareMessage
 	 *  payloadSize    | -                                          | in: the size of the payload. This field MUST be initialized between functions call
 	 *  payloadType    | in: the type of the payload                | -
-	 *  position       | in: current position. not needed for CAM   | -
-	 *  generationTime | in:                                        | -
-	 *  expirationTime | - unsupported                              | - unsupported
+	 *  position       | in: current position. Not needed for CAM   | -
+	 *  generationTime | in: the timestamp of the last GPS fix      | -
+	 *  expirationTime | - unsupported                              | - 
 	 *  si_type        | out: signer info of the message            | in: must be the same as returned from FitSec_PrepareMessage
 	 *  cert           | out: certificate to be used to sign message| in: the same value as returned from FitSec_PrepareMessage
-	 *  ssp            |  -                                         | -
+	 *  ssp            | in: the ssp bits representing content of   | -
+	 *                 |     the message. These bits will be used to|
+	 *                 |     select proper certificate.             |
 	 */
 	FITSEC_EXPORT int FitSec_PrepareMessage(FitSec * e, FSMessageInfo* m, char * buf, int bufsize);
 	FITSEC_EXPORT int FitSec_SignMessage(FitSec * e, FSMessageInfo* m, char * buf, int bufsize);
 
 	/** Verify ITS message.
-         *  @param e       [In]    The engine
-         *  @param info    [In/Out]Message information.
-         *  @param msg     [In]Pointer to the buffer of the message to be verified
-         *  @param msgize  [In]The size of the buffer
+         *  @param e       [In]      The engine
+         *  @param info    [In/Out]  Message information.
+         *  @param msg     [In]      Pointer to the buffer of the message to be verified
+         *  @param msgize  [In]      The size of the buffer
          *  @return        FSTRUE for Success or FSFALSE for error. See info->status for error value
+         *
          *  Description of fields in FSMessageInfo:
 	 *                 | FitSec_Verify
 	 *  ---------------|--------------------------------------------
-	 *  payload        | out: returns begin of payload element       
-	 *  payloadSize    | out: the size of message payload                                          
-	 *  payloadType    | out: the type of the payload                
-	 *  position       | out: the position where message has been signed
-	 *  generationTime | out: the time when message has been generated                                       
-	 *  expirationTime | - unsupported now                              
-	 *  si_type        | out: signer info of the message            
+	 *  payload        | out: points to the first byte of payload
+	 *  payloadSize    | out: the size of message payload
+	 *  payloadType    | out: the type of the payload
+	 *  position       | out: the remote position where message has been signed
+	 *  generationTime | out: the time when message has been generated
+	 *  expirationTime | - unsupported for now
+	 *  si_type        | out: signer info of the message
 	 *  cert           | out: the AT certificate that has been used to sign message
-	 *  ssp            | out: the AID of the message and SSP of the certificate                                         
+	 *  ssp            | out: the AID of the message and SSP of the certificate
 	 */
 	FITSEC_EXPORT FSBOOL FitSec_Verify(FitSec * e, FSMessageInfo * info, const void * msg, int msgsize);
 	
-	/** Returns the message corresponding to the error value */
-	FITSEC_EXPORT const char * FitSec_ErrorMessage(int err);
-
 #ifdef __cplusplus
 }
 #endif
